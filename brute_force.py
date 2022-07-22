8a0260df80460d31568d28b1eaae19d8cf0cec27
Vitaliy Osidach <vitaliy.osidach@ihsmarkit.com>
2022-07-22 09:52:18 +0300

    final version of brute force

--- brute_force.py
+++ brute_force.py
@@ -1,25 +1,38 @@
-import getpass4
+# import getpass
 import string
 from time import perf_counter
 
-passwd = getpass4.getpass(prompt='Password: ', char='-')
-# passwd = 'Acer5'
-dct = string.ascii_lowercase + string.ascii_uppercase + string.digits
+# passwd = getpass.getpass(prompt='Password: ', char='-')
+scope_st = string.ascii_lowercase + string.ascii_uppercase + string.digits
+last_symbol = scope_st[-1]
+first_symbol = scope_st[0]
+passwd = 'Acer'
 
+
+def timer(func):
+    def timeit(*args, **kwargs):
+        begin = perf_counter()
+        func(*args, **kwargs)
+        end = perf_counter()
+        print(f"Total time taken in {func.__name__}:  {end - begin} sec")
+
+    return timeit
 
 
 def increase(wrd: string) -> str:
     if len(wrd) == 0:
-        return dct[0]
-    if wrd[-1] == dct[-1]:
-        return increase(wrd[:-1]) + dct[0]
-    return wrd[:-1] + dct[dct.index(wrd[-1]) + 1]
+        return first_symbol
+    if wrd[-1] == last_symbol:
+        return increase(wrd[:-1]) + first_symbol
+    return wrd[:-1] + scope_st[scope_st.index(wrd[-1]) + 1]
 
 
-def calc(wrd):
-    while len(wrd) < 12:
+@timer
+def calc():
+    wrd = ''
+    while len(wrd) < 6:
         # print(f"Checking: {wrd + '_'}")
-        for i in dct:
+        for i in scope_st:
             if wrd + i == passwd:
                 print(f"Password is: {wrd + i}")
                 return
@@ -28,7 +41,4 @@ def calc(wrd):
 
 
 if __name__ == "__main__":
-    cur_time = perf_counter()
-    calc('')
-    end_time = perf_counter()
-    print(f'Elapsed time (s): {end_time - cur_time}')
+    calc()
